06d6f9007e4a7a6bec3242b86c1b4b5c24a50559
Dirwul <dirwulviques@gmail.com>
2025-06-17 18:50:42 +0300

    fix ssl cert verification

--- src/main/java/tech/dirwul/grande/api/SSLVerificationController.java
+++ src/main/java/tech/dirwul/grande/api/SSLVerificationController.java
@@ -1,12 +1,14 @@
 package tech.dirwul.grande.api;
 
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 public class SSLVerificationController {
@@ -15,15 +17,16 @@ public class SSLVerificationController {
 	private static final String VALIDATION_PATH = "/.well-known/pki-validation/" + VALIDATION_FILE_NAME;
 
 	@GetMapping(VALIDATION_PATH)
-	public ResponseEntity<ByteArrayResource> verify() {
-		File file = new File("src/main/resources/static/" + VALIDATION_FILE_NAME);
-		byte[] contentBytes = file.toString().getBytes();
-		ByteArrayResource resource = new ByteArrayResource(contentBytes);
+	public ResponseEntity<ByteArrayResource> verify() throws IOException {
+		ClassPathResource resource = new ClassPathResource("static/" + VALIDATION_FILE_NAME);
+		byte[] contentBytes = Files.readAllBytes(resource.getFile().toPath());
+		ByteArrayResource byteArrayResource = new ByteArrayResource(contentBytes);
+
 		return ResponseEntity.ok()
 			.contentType(MediaType.parseMediaType("application/octet-stream"))
 			.contentLength(contentBytes.length)
 			.contentType(MediaType.TEXT_PLAIN)
-			.body(resource);
+			.body(byteArrayResource);
 	}
 
 }
